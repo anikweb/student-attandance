12b08ebcac287ecd269bfe5e1a8dce56b2a65150
anikweb <aniknandi.it@gmail.com>
2022-09-18 23:12:13 +0600

    first commit

---
+++ inc/header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Twitter -->
+    <meta name="twitter:site" content="@themepixels">
+    <meta name="twitter:creator" content="@themepixels">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Starlight">
+    <meta name="twitter:description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="twitter:image" content="http://themepixels.me/starlight/img/starlight-social.png">
+
+    <!-- Facebook -->
+    <meta property="og:url" content="http://themepixels.me/starlight">
+    <meta property="og:title" content="Starlight">
+    <meta property="og:description" content="Premium Quality and Responsive UI for Dashboard.">
+
+    <meta property="og:image" content="http://themepixels.me/starlight/img/starlight-social.png">
+    <meta property="og:image:secure_url" content="http://themepixels.me/starlight/img/starlight-social.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="600">
+
+    <!-- Meta -->
+    <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="author" content="ThemePixels">
+
+    <title>Student Attandance</title>
+
+    <!-- vendor css -->
+    <link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="lib/Ionicons/css/ionicons.css" rel="stylesheet">
+    <link href="lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+    <link href="lib/rickshaw/rickshaw.min.css" rel="stylesheet">
+    <!-- Starlight CSS -->
+    <link rel="stylesheet" href="css/starlight.css">
+  </head>
+
+  <body>
+
+    <!-- ########## START: LEFT PANEL ########## -->
+    <div class="sl-logo"><a href=""><i class="icon ion-android-star-outline"></i>Teacher</a></div>
+    <div class="sl-sideleft">
+      <label class="sidebar-label">Navigation</label>
+      <div class="sl-sideleft-menu"> ;
+        <a href="" class="sl-menu-link active">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+            <span class="menu-item-label">Dashboard</span>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+        <a href="#" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon icon ion-ios-people tx-24"></i>
+            <span class="menu-item-label">Students</span>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+        <a href="#" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon icon ion-ios-plus tx-24"></i>
+            <span class="menu-item-label">Add Student</span>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+        <a href="#" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon icon ion-ios-plus tx-24"></i>
+            <span class="menu-item-label">Take Attandance</span>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+      </div><!-- sl-sideleft-menu -->
+    </div><!-- sl-sideleft -->
+    <!-- ########## END: LEFT PANEL ########## -->
---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+require_once('inc/header.php');
+?>
+    <!-- ########## START: HEAD PANEL ########## -->
+    <div class="sl-header">
+      <div class="sl-header-left">
+        <div class="navicon-left hidden-md-down"><a id="btnLeftMenu" href=""><i class="icon ion-navicon-round"></i></a></div>
+        <div class="navicon-left hidden-lg-up"><a id="btnLeftMenuMobile" href=""><i class="icon ion-navicon-round"></i></a></div>
+      </div><!-- sl-header-left -->
+      <div class="sl-header-right">
+        <nav class="nav">
+          <div class="dropdown">
+            <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
+              <span class="logged-name">Jane<span class="hidden-md-down"> Doe</span></span>
+              <img src="../img/img3.jpg" class="wd-32 rounded-circle" alt="">
+            </a>
+            <div class="dropdown-menu dropdown-menu-header wd-200">
+              <ul class="list-unstyled user-profile-nav">
+                <li><a href=""><i class="icon ion-ios-person-outline"></i> Edit Profile</a></li>
+                <li><a href=""><i class="icon ion-ios-gear-outline"></i> Settings</a></li>
+                <li><a href=""><i class="icon ion-ios-download-outline"></i> Downloads</a></li>
+                <li><a href=""><i class="icon ion-ios-star-outline"></i> Favorites</a></li>
+                <li><a href=""><i class="icon ion-ios-folder-outline"></i> Collections</a></li>
+                <li><a href=""><i class="icon ion-power"></i> Sign Out</a></li>
+              </ul>
+            </div><!-- dropdown-menu -->
+          </div><!-- dropdown -->
+        </nav>
+        <div class="navicon-right">
+          <a id="btnRightMenu" href="" class="pos-relative">
+            <i class="icon ion-ios-bell-outline"></i>
+            <!-- start: if statement -->
+            <span class="square-8 bg-danger"></span>
+            <!-- end: if statement -->
+          </a>
+        </div><!-- navicon-right -->
+      </div><!-- sl-header-right -->
+    </div><!-- sl-header -->
+    <!-- ########## END: HEAD PANEL ########## -->
+    <!-- ########## START: MAIN PANEL ########## -->
+    <div class="sl-mainpanel">
+      <nav class="breadcrumb sl-breadcrumb">
+        <a class="breadcrumb-item" href="index.html">Student Attandance</a>
+        <span class="breadcrumb-item active">Dashboard</span>
+      </nav>
+
+      <div class="sl-pagebody">
+
+        <div class="row row-sm">
+          <div class="col-sm-6 col-xl-3">
+            <div class="card pd-20 bg-primary">
+              <div class="d-flex justify-content-between align-items-center mg-b-10">
+                <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Total Students</h6>
+                <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
+              </div><!-- card-header -->
+              <div class="d-flex align-items-center justify-content-between">
+                <h3 class="mg-b-0 tx-white tx-lato tx-bold">0</h3>
+              </div><!-- card-body -->
+            </div><!-- card -->
+          </div><!-- col-3 -->
+          <div class="col-sm-6 col-xl-3 mg-t-20 mg-xl-t-0">
+            <div class="card pd-20 bg-purple">
+              <div class="d-flex justify-content-between align-items-center mg-b-10">
+                <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Today Present</h6>
+                <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
+              </div><!-- card-header -->
+              <div class="d-flex align-items-center justify-content-between">
+                <h3 class="mg-b-0 tx-white tx-lato tx-bold">0</h3>
+              </div><!-- card-body -->
+            </div><!-- card -->
+          </div><!-- col-3 -->
+          <div class="col-sm-6 col-xl-3 mg-t-20 mg-xl-t-0">
+            <div class="card pd-20 bg-sl-primary">
+              <div class="d-flex justify-content-between align-items-center mg-b-10">
+                <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Total Present (This Month)</h6>
+                <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
+              </div><!-- card-header -->
+              <div class="d-flex align-items-center justify-content-between">
+                <h3 class="mg-b-0 tx-white tx-lato tx-bold">0</h3>
+              </div><!-- card-body -->
+              </div><!-- -->
+            </div><!-- card -->
+          </div><!-- col-3 -->
+        </div><!-- row -->
+      </div><!-- sl-pagebody -->
+    </div><!-- sl-mainpanel -->
+    <!-- ########## END: MAIN PANEL ########## -->
+
+    <script src="lib/jquery/jquery.js"></script>
+    <script src="lib/popper.js/popper.js"></script>
+    <script src="lib/bootstrap/bootstrap.js"></script>
+    <script src="lib/jquery-ui/jquery-ui.js"></script>
+    <script src="lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
+    <script src="lib/jquery.sparkline.bower/jquery.sparkline.min.js"></script>
+    <script src="lib/d3/d3.js"></script>
+    <script src="lib/rickshaw/rickshaw.min.js"></script>
+    <script src="lib/chart.js/Chart.js"></script>
+    <script src="lib/Flot/jquery.flot.js"></script>
+    <script src="lib/Flot/jquery.flot.pie.js"></script>
+    <script src="lib/Flot/jquery.flot.resize.js"></script>
+    <script src="lib/flot-spline/jquery.flot.spline.js"></script>
+
+    <script src="js/starlight.js"></script>
+    <script src="js/ResizeSensor.js"></script>
+    <script src="js/dashboard.js"></script>
+  </body>
+</html>
